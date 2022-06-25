325dc0c1ddba1444d376470e06c104216c0c396d
leiningbo <1071781349@qq.com>
2022-06-25 20:31:12 +0800

    feat(cloud学习): 全局统一数据返回

---
+++ cloud-provider-payment8001/src/main/java/com/abo/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.abo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: abo-cloud
+ * @description:
+ * @author: leiningbo
+ * @create: 2022-06-25 20:08
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
---
+++ cloud-provider-payment8001/src/main/java/com/abo/springcloud/entity/Payment.java
@@ -0,0 +1,17 @@
+package com.abo.springcloud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    /**
+     *
+     */
+    private Long id;
+
+    private String serial;
+}
