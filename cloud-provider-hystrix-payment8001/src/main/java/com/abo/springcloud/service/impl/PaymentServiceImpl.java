0a6b06c5547b1661b57cdc26f199f40a3479559b
leiningbo <1071781349@qq.com>
2022-06-29 22:53:30 +0800

    feat(cloud学习): hystrix服务降级 @HystrixCommand  @EnableCircuitBreaker

--- cloud-provider-hystrix-payment8001/src/main/java/com/abo/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/abo/springcloud/service/impl/PaymentServiceImpl.java
@@ -4,6 +4,8 @@ import com.abo.springcloud.entity.Payment;
 import com.abo.springcloud.mapper.PaymentMapper;
 import com.abo.springcloud.service.PaymentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -22,14 +24,22 @@ public class PaymentServiceImpl extends ServiceImpl<PaymentMapper, Payment> impl
         return "xxx" + Thread.currentThread().getName() + "__ok,id:" + id + "\t";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties ={
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")} )
     @Override
     public String paymentInfoTimeOut(Integer id) {
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "xxx" + Thread.currentThread().getName() + "__ok,id:" + id + "\t" + "增加时长3秒";
+        int time = 5;
+        int age = 10 / 0;
+//        try {
+//            TimeUnit.SECONDS.sleep(time);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return "xxx" + Thread.currentThread().getName() + "paymentInfoTimeOut__ok,id:" + id + "\t" + "耗时秒：" + time;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池" + Thread.currentThread().getName() + "系统繁忙paymentInfo_TimeOutHandler,id:" + id + "\t" + "O(∩_∩)O哈哈~";
     }
 
 }
