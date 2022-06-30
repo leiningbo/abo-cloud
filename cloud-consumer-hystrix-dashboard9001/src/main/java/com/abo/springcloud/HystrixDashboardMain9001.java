9b18f4204fa7c568e25ef208b553dd019d01fc42
leiningbo <1071781349@qq.com>
2022-06-30 22:18:39 +0800

    feat(cloud学习): 增加Hystrix Dashboard 图形化

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/abo/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.abo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @program: abo-cloud
+ * @description:
+ * @author: leiningbo
+ * @create: 2022-06-30 22:15
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+
+}
