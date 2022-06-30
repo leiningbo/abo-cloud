345bb3ecbfafed6a40980aad9840077fc6c5d26f
leiningbo <1071781349@qq.com>
2022-06-30 23:53:37 +0800

    feat(cloud学习): cloud-geteway 定时路由

---
+++ cloud-gateway-gateway9527/src/test/java/Test1.java
@@ -0,0 +1,19 @@
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
+
+/**
+ * @program: abo-cloud
+ * @description:
+ * @author: leiningbo
+ * @create: 2022-06-30 23:51
+ **/
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+
+    }
+
+}
